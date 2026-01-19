aa589032ae8734c5ff6c7d4d98e3d8e4f4dfbeac
alex <alexnaf199@gmail.com>
2026-01-19 11:59:55 +0300

    feat:  crud папок

--- src/folders/folders.controller.ts
+++ src/folders/folders.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { FoldersService } from './folders.service';
 import { AuthStrictGuard } from 'src/guards/authStrict.guard';
 import { Request as ExpressRequest } from 'express';
@@ -15,5 +15,66 @@ export class FoldersController {
   async getFolders(@Req() req: RequestWithUser) {
     const { userId } = req.user;
     return this.foldersService.getFolders(userId);
+  }
+
+  @Post('/create')
+  @UseGuards(AuthStrictGuard)
+  async CreateFolder(@Req() req: RequestWithUser) {
+    const { name, x, y, parentId, type, content } = req.body;
+
+    const { userId } = req.user;
+
+    return this.foldersService.createFolder(
+      name,
+      x,
+      y,
+      parentId,
+      type,
+      content,
+      userId,
+    );
+  }
+
+  @Post('/clear-bin')
+  @UseGuards(AuthStrictGuard)
+  async clearBin(@Req() req: RequestWithUser) {
+    const { ids } = req.body;
+
+    return this.foldersService.clearBin(ids);
+  }
+
+  @Put('/move')
+  @UseGuards(AuthStrictGuard)
+  async moveFolder(@Req() req: RequestWithUser) {
+    const { id, newX, newY } = req.body;
+    const { userId } = req.user;
+
+    return this.foldersService.moveFolder(id, newX, newY, userId);
+  }
+
+  @Put('/rename')
+  @UseGuards(AuthStrictGuard)
+  async renameFolder(@Req() req: RequestWithUser) {
+    const { id, newName } = req.body;
+    const { userId } = req.user;
+
+    return this.foldersService.renameFolder(id, newName, userId);
+  }
+
+  @Put('/move-to-folder')
+  @UseGuards(AuthStrictGuard)
+  async moveToFolder(@Req() req: RequestWithUser) {
+    const { id, parentId, x, y } = req.body;
+    const { userId } = req.user;
+
+    return this.foldersService.moveToFolder(id, parentId, x, y, userId);
+  }
+
+  @Put('/save-text')
+  @UseGuards(AuthStrictGuard)
+  async saveText(@Req() req: RequestWithUser) {
+    const { id, content } = req.body;
+
+    return this.foldersService.saveText(id, content);
   }
 }
